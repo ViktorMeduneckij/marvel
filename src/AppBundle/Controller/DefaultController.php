0357a53eb39a2be1216075dcbab11051c4966559
Viktor Meduneckij <viktor@adapt.dk>
2017-07-20 15:41:57 +0300

    MARVEL-1 implementation of search bar connected to the API, no styling

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Hero;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class DefaultController extends Controller
 {
@@ -13,13 +19,30 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $this->connectToApi();
-        $this->retrieveByName('spider', $this->connectToApi());
+      $heroName = $request->request->get('search');
+      $this->connectToApi();
+      if(!empty($heroName)) {
+        $heroes = $this->retrieveByName($heroName, $this->connectToApi());
+        return $this->render('home/homepage.html.twig', array(
+          'heroes' => $heroes,
+        ));
+      }
+
+
+
+      return $this->render('home/homepage.html.twig');
     }
 
-    public function connectToApi() {
-      //Create a TimeStamp
+    public function getHeroRepo()
+    {
+        $em = $this-> getDoctrine()->getManager();
+        $repository = $em->getRepository('AppBundle:Hero');
+        return $repository;
+    }
 
+    public function connectToApi()
+    {
+      //Create a TimeStamp
       $ts = time();
       $public_key = '4a23db65667e6501465d203a11696099';
       $private_key = '8cd5f6fa634d1764610398f1583ef885f1cfef01';
@@ -37,17 +60,50 @@ class DefaultController extends Controller
       return $query;
     }
 
-    public function retrieveByName($name, $query) {
+    public function retrieveByName($name, $query)
+    {
       //make the request
       $response = file_get_contents('http://gateway.marvel.com/v1/public/characters?nameStartsWith=' . $name . '&' . $query);
 
       //convert the json string to an array
       $response_data = json_decode($response, true);
 
-      $hero_name = $response_data['data']['results'][4]['name'];
-      $hero_description = $response_data['data']['results'][4]['description'];
+      $results = $response_data['data']['results'];
 
-      var_dump($hero_name, $hero_description);die;
-
+      //Define a new array in which I will store all of the heroes and their info returned by API
+      $all_heroes = array();
+      for($i = 0; $i < count($results); $i++) {
+          $hero[$i] = new Hero();
+          $hero[$i]->setName($results[$i]['name']);
+          $hero[$i]->setDescription($results[$i]['description']);
+          $hero[$i]->setImageUrl($results[$i]['thumbnail']['path']);
+          $hero[$i]->setImageExtension($results[$i]['thumbnail']['extension']);
+          array_push($all_heroes, $hero[$i]);
+      }
+      return $all_heroes;
     }
+
+    public function saveHero($hero)
+    {
+      $em = $this->getDoctrine()->getManager();
+
+      //Check if hero with the same name exists, if it does, don't push to database
+      $old_hero = $this->getDoctrine()
+        ->getRepository(Hero::class)
+        ->find($hero->getName());
+
+      if(!empty($old_hero)) {
+        $em->persist($hero);
+        $em->flush();
+        return new Response(
+          '<html><body>Good</body></html>'
+        );
+      }
+      else {
+        return new Response(
+          '<html><body>This exists son</body></html>'
+        );
+      }
+    }
+
 }
